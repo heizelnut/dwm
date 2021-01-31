ec8300c0b9e25d4164caeeea399c52a8bfeaf684
heizelnut <upbeatshaw@protonmail.com>
2021-01-31 18:22:57 +0100

    🔳 Pimp my borders

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -17,7 +17,7 @@ static const char lightest[]        = "#444444";
 static const char *colors[][3] = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { lightgray, black, darkgray },
-	[SchemeSel]  = { white, black, lightest },
+	[SchemeSel]  ={ white, black, lightgray },
 };
 
 /* tagging */
